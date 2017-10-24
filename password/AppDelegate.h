db6a0250e1255d24b3c807e11cbef66278731e59
hoomsun <>
2017-10-24 15:55:02 +0800

    first commit

---
+++ password/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  password
+//
+//  Created by hoomsun on 2017/10/24.
+//  Copyright © 2017年 hoomsun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
